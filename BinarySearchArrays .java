3726bdc55146ca09d7746a0615fe1a29c90012c9
Mahboob Attar <134129628+Mahboob-Attar@users.noreply.github.com>
2025-09-22 01:03:50 +0530

    Update BinarySearchArrays .java

--- BinarySearchArrays .java
+++ BinarySearchArrays .java
@@ -2,18 +2,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinarySearchArrays {
-    public static int searchBinary(int [] arr1,int target) {
+    public static int searchBinary(int [] arr,int target) {
         int low = 0;
-        int high = arr1.length - 1;
+        int high = arr.length - 1;
+        boolean isAssending = arr[0] <= arr[arr.length-1];
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (arr1[mid] == target) {
+            if (arr[mid] == target) {
                 return mid;
-            } else if (arr1[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            }if(isAssending){
+                if (arr[mid] < target) {
+                    low = mid + 1;
+                }else {
+                    high = mid - 1;
+                }
+            }else{
+                    if(arr[mid]>target){
+                        low = mid+1;
+                    }else{
+                        high= mid-1;
+                    }
+                }
         }
         return -1;
     }
@@ -26,7 +35,7 @@ public class BinarySearchArrays {
         for(int i=0;i<n; i++){
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
+       // Arrays.sort(arr); this only sort asc if u want desc must use INTEGER[] with collection.reverse()
         System.out.println("enter target value");
         int target = sc.nextInt();
         int idx = searchBinary(arr,target);
